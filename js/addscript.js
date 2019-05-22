4d3f49b92716fc44f22a8a84eec8d23866e57a8a
okomaro <okomaro@gmail.com>
2019-05-23 00:11:26 +0200

    menu fix

--- js/addscript.js
+++ js/addscript.js
@@ -45,14 +45,15 @@
 
     // Collapse now if page is not at top
     navbarCollapse();
+
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
 
     // Hide navbar when modals trigger
-    $('.portfolio-modal').on('show.bs.modal', function(e) {
+    $('.practical-modal').on('show.bs.modal', function(e) {
         $('.navbar').addClass('d-none');
     })
-    $('.portfolio-modal').on('hidden.bs.modal', function(e) {
+    $('.practical-modal').on('hidden.bs.modal', function(e) {
         $('.navbar').removeClass('d-none');
     })
 
